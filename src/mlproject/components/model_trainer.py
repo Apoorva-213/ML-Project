38f83ad09eee73118121a058e2afd3978306c72d
Apoorva-213 <a1p2oorva2132005@gmail.com>
2025-08-08 21:30:33 +0530

    Model Trainer

---
+++ src/mlproject/components/model_trainer.py
@@ -0,0 +1,70 @@
+import os
+import sys
+from dataclasses import dataclass
+
+from catboost import CatBoostRegressor
+from sklearn.ensemble import(
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor
+)
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+
+
+from mlproject.exception import CustomException
+from mlproject.logger import logging
+from mlproject.utils import save_object
+from mlproject.utils import evaluate_models
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path=os.path.join('artifacts','model.pkl')
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config=ModelTrainerConfig()
+
+    def initiate_model_trainer(self,train_array,test_array):
+        try:
+            logging.info("splitting training and test input data")
+            x_train,y_train,x_test,y_test=(
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
+            )
+            models={
+                "Random Forest":RandomForestRegressor(),
+                "Decision Tree":DecisionTreeRegressor(),
+                "Gradient Boosting":GradientBoostingRegressor(),
+                "Linear Regression":LinearRegression(),
+                "K -Neighbours Classifier":KNeighborsRegressor(),
+                "Catboost Classifier":CatBoostRegressor(),
+                "Adaboost Classifier":AdaBoostRegressor()
+            }
+
+            model_report:dict=evaluate_models(x_train,y_train,x_test,y_test,models)
+            
+            best_model_score=max(sorted(model_report.values()))
+
+            best_model_name=list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+
+            best_model=models[best_model_name]
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+            predicted=best_model.predict(x_test)
+
+            r2score=r2_score(y_test,predicted)
+            return r2score
+            
+        except Exception as e:
+            raise CustomException(e,sys)
